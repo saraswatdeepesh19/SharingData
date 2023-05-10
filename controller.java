34bbae37e18fa882a818ef79d95e19b82259981b
Deepesh Saraswat <37635753+saraswatdeepesh19@users.noreply.github.com>
2023-05-10 15:42:00 +0530

    Update controller.java

--- controller.java
+++ controller.java
@@ -5,31 +5,37 @@ public class HmlIosDevicesController {
     @Autowired
     private HmlIosDevicesService service;
 
+    // Create a new device
     @PostMapping("/")
     public HmlIosDevices create(@RequestBody HmlIosDevices device) {
         return service.create(device);
     }
 
+    // Get a device by udid
     @GetMapping("/{udid}")
     public HmlIosDevices read(@PathVariable String udid) {
         return service.read(udid);
     }
 
+    // Update a device by udid
     @PutMapping("/{udid}")
     public HmlIosDevices update(@PathVariable String udid, @RequestParam(name = "psid", required = false) String psid) {
         return service.update(udid, psid);
     }
 
+    // Delete a device by udid
     @DeleteMapping("/{udid}")
     public void delete(@PathVariable String udid) {
         service.delete(udid);
     }
 
+    // Get a list of all devices
     @GetMapping("/")
     public List<HmlIosDevices> list() {
         return service.list();
     }
 
+    // Handle exceptions
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleException(RuntimeException e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
