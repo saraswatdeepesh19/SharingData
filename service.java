4b72dd041b77ab7ac0a143d828a3ae65eda9db2a
Deepesh Saraswat <37635753+saraswatdeepesh19@users.noreply.github.com>
2023-05-10 15:42:46 +0530

    Update service.java

--- service.java
+++ service.java
@@ -4,35 +4,45 @@ public class HmlIosDevicesService {
     @Autowired
     private HmlIosDevicesRepository repository;
 
+    // Create a new device
     public HmlIosDevices create(HmlIosDevices device) {
         return repository.save(device);
     }
 
+    // Get a device by udid
     public HmlIosDevices read(String udid) {
         return repository.findById(udid).orElseThrow(() -> new RuntimeException("Device with udid " + udid + " not found"));
     }
 
+    // Update a device by udid
     public HmlIosDevices update(String udid, String psid) {
+        // Find the device by udid
         HmlIosDevices device = repository.findById(udid).orElseThrow(() -> new RuntimeException("Device with udid " + udid + " not found"));
 
+        // If the device already has a psid, set it to null and mark status as online
         if (device.getPsid() != null) {
             device.setPsid(null);
             device.setStatus("online");
         } else {
+            // If the device doesn't have a psid, mark status as offline
             device.setStatus("offline");
         }
 
+        // If a psid is provided, update the device's psid
         if (psid != null) {
             device.setPsid(psid);
         }
 
+        // Save the updated device to the database
         return repository.save(device);
     }
 
+    // Delete a device by udid
     public void delete(String udid) {
         repository.deleteById(udid);
     }
 
+    // Get a list of all devices
     public List<HmlIosDevices> list() {
         return repository.findAll();
     }
