a4e4b74efb5080a7a49129d44f46b8e9b93cbe84
Deepesh Saraswat <37635753+saraswatdeepesh19@users.noreply.github.com>
2024-04-04 15:25:00 +0530

    Update TestJdbc.java

--- TestJdbc.java
+++ TestJdbc.java
@@ -1,34 +1,50 @@
-package src.com.jdbc;
+<!DOCTYPE html>
+<html lang="en" xmlns:th="http://www.thymeleaf.org">
+<head>
+    <meta charset="UTF-8">
+    <title>User Management</title>
+</head>
+<body>
+<h1>User Management</h1>
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.Enumeration;
+<!-- Display User Count Data -->
+<table border="1">
+    <tr>
+        <th>Month</th>
+        <th>Year</th>
+        <th>User Count</th>
+    </tr>
+    <tr th:each="user : ${users}">
+        <td th:text="${user.month}"></td>
+        <td th:text="${user.year}"></td>
+        <td th:text="${user.userCount}"></td>
+    </tr>
+</table>
 
-public class TestJdbc {
+<!-- Form to Save User Count Data -->
+<form action="#" th:action="@{/save}" method="post">
+    <label for="month">Month:</label>
+    <input type="number" id="month" name="month" required>
+    <br>
+    <label for="year">Year:</label>
+    <input type="number" id="year" name="year" required>
+    <br>
+    <label for="userCount">User Count:</label>
+    <input type="number" id="userCount" name="userCount" required>
+    <br>
+    <button type="submit">Save</button>
+</form>
 
-	public static void main(String[] args) {
-		
-		String url = "jdbc:oracle:thin:@127.0.0.1:1521:";
-		String user ="SYSTEM";
-		String password = "root";
-		
-		try {
-			System.out.println("Connecting to DAtabase");
-		//	Class.forName("oracle.jdbc.OracleDriver");
-			Connection connection = DriverManager.getConnection(url, user, password);
-			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery("Select 1 from dual");
-			while (resultSet.next()) {
-				System.out.println(resultSet.getString(1));				
-			}
-		      System.out.println("Connection Success");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+<!-- Link to Show Data Between Two Dates -->
+<form action="#" th:action="@{/data}" method="get">
+    <label for="startDate">Start Date:</label>
+    <input type="date" id="startDate" name="startDate" required>
+    <br>
+    <label for="endDate">End Date:</label>
+    <input type="date" id="endDate" name="endDate" required>
+    <br>
+    <button type="submit">Show Data</button>
+</form>
 
-	}
-
-}
+</body>
+</html>
