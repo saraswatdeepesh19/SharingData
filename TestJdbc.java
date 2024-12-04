1b643bbd828f53e77d4aa4033e0ab54696b95d83
Deepesh Saraswat <37635753+saraswatdeepesh19@users.noreply.github.com>
2024-12-04 13:39:31 +0530

    Update TestJdbc.java

--- TestJdbc.java
+++ TestJdbc.java
@@ -1,1 +1,14 @@
 logging.level.org.springframework.cloud.gateway.filter.factory.RewritePathGatewayFilterFactory=DEBUG
+server:
+  port: 1234
+
+spring:
+  cloud:
+    gateway:
+      routes:
+        - id: user-management-route
+          uri: http://hkw2501ABCD:8007 # Correct target service
+          predicates:
+            - Path=/v1/api/user-management/**
+          filters:
+            - RewritePath=/v1/api/user-management/(?<segment>.*), /user/${segment}
